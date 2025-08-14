bc8b760ffaea3fa6ce0ba5c307f71d1376cabed2
Shukurullo <shukurulloshokiraliyev699@gmail.com>
2025-08-14 16:59:21 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,225 @@
+//CARDS_PARENT
+const CardsEl = document.getElementById("Cards");
+CardsEl.style.cssText = `
+    width: 950px;
+height: 500px;
+top: 171px;
+left: 260px;
+opacity: 1;
+border-radius: 8px;
+margin: 200px 0px 200px 300px;
+display:flex;
+gap:-5px;
+`;
+//ICONS
+const icon1El = document.getElementById("icon1");
+icon1El.style.cssText = `
+margin: 48px 195px 6px 48px ;
+`;
+const icon2El = document.getElementById("icon2");
+icon2El.style.cssText = `
+margin: 48px 195px 6px 48px ;
+`;
+const icon3El = document.getElementById("icon3");
+icon3El.style.cssText = `
+margin: 48px 195px 6px 48px ;
+`;
+//CARD1
+const CardEl = document.getElementById("card1");
+CardEl.style.cssText = `
+    width: 920px;
+    height: 500px;
+    opacity: 1;
+    top: 171px;
+    left: 260px;
+    border-radius: 8px;
+    background-color: #E28625;
+`; //CARD2
+const Card2El = document.getElementById("card2");
+Card2El.style.cssText = `
+    width: 920px;
+    height: 500px;
+    opacity: 1;
+    top: 171px;
+    left: 260px;
+    border-radius: 8px;
+    background-color:#006971;
+`;
+//CARD3
+const Card3El = document.getElementById("card3");
+Card3El.style.cssText = `
+    width: 920px;
+    height: 500px;
+    opacity: 1;
+    top: 171px;
+    left: 260px;
+    border-radius: 8px;
+    background-color:#004140;
+`;
+//H's
+const H1El = document.getElementById("h1");
+
+H1El.style.cssText = `
+  font-family: "Big Shoulders Display", sans-serif;
+  font-weight: 700;
+  font-size: 40px;
+  leading-trim: none;
+  line-height: 100%;
+  letter-spacing: 0px;
+  text-transform: uppercase;
+  margin: 25px 160px 26px 48px;
+  color: #F2F2F2;
+`;
+
+const H2El = document.getElementById("h2");
+
+H2El.style.cssText = `
+  font-family: "Big Shoulders Display", sans-serif;
+  font-weight: 700;
+  font-size: 40px;
+  leading-trim: none;
+  line-height: 100%;
+  letter-spacing: 0px;
+  text-transform: uppercase;
+  margin: 25px 160px 26px 48px;
+  color: #F2F2F2;
+`;
+
+const H3El = document.getElementById("h3");
+
+H3El.style.cssText = `
+  font-family: "Big Shoulders Display", sans-serif;
+  font-weight: 700;
+  font-size: 40px;
+  leading-trim: none;
+  line-height: 100%;
+  letter-spacing: 0px;
+  text-transform: uppercase;
+  margin: 25px 160px 26px 48px;
+  color: #F2F2F2;
+`;
+
+//PCHALAR
+
+const P1El = document.getElementById("p1");
+
+P1El.style.cssText = `
+  font-family: "Lexend Deca", sans-serif;
+  font-weight: 400;
+  font-size: 15px;
+  leading-trim: none;
+  line-height: 25px;
+  letter-spacing: 0px;
+  width: 212px;
+  height: 125px;
+  opacity: 0.75;
+  top: 367px;
+  left: 307.5px;
+  color: #FFFFFF;
+  margin: 0 47px 83px 47px;
+`;
+
+const P2El = document.getElementById("p2");
+
+P2El.style.cssText = `
+  font-family: "Lexend Deca", sans-serif;
+  font-weight: 400;
+  font-size: 15px;
+  leading-trim: none;
+  line-height: 25px;
+  letter-spacing: 0px;
+  width: 212px;
+  height: 125px;
+  opacity: 0.75;
+  top: 367px;
+  left: 307.5px;
+  color: #FFFFFF;
+  margin: 0 47px 83px 47px;
+`;
+const P3El = document.getElementById("p3");
+
+P3El.style.cssText = `
+  font-family: "Lexend Deca", sans-serif;
+  font-weight: 400;
+  font-size: 15px;
+  leading-trim: none;
+  line-height: 25px;
+  letter-spacing: 0px;
+  width: 212px;
+  height: 125px;
+  opacity: 0.75;
+  top: 367px;
+  left: 307.5px;
+  color: #FFFFFF;
+  margin: 0 47px 83px 47px;
+`;
+
+//BUTTONS
+// 1-button
+const BUT1El = document.getElementById("button1");
+const Pcha1El = document.getElementById("pInButton1");
+
+BUT1El.style.cssText = `
+  width: 146px;
+  height: 48px;
+  opacity: 1;
+  border-radius: 25px;
+  border: none;
+      margin: 0 113px 48px 48px;
+`;
+Pcha1El.style.cssText = `
+  font-family: "Lexend Deca", sans-serif;
+  font-weight: 400;
+  font-size: 15px;
+  leading-trim: none;
+  line-height: 25px;
+  letter-spacing: 0px;
+  color: #E28625;
+    margin:auto;
+`;
+
+// 2-button
+const BUT2El = document.getElementById("button2");
+const Pcha2El = document.getElementById("pInButton2");
+
+BUT2El.style.cssText = `
+  width: 146px;
+  height: 48px;
+  opacity: 1;
+  border-radius: 25px;
+  border: none;
+      margin: 0 113px 48px 48px;
+`;
+Pcha2El.style.cssText = `
+  font-family: "Lexend Deca", sans-serif;
+  font-weight: 400;
+  font-size: 15px;
+  leading-trim: none;
+  line-height: 25px;
+  letter-spacing: 0px;
+  color: #006971;
+    margin:auto;
+`;
+
+// 3-button
+const BUT3El = document.getElementById("button3");
+const Pcha3El = document.getElementById("pInButton3");
+
+BUT3El.style.cssText = `
+  width: 146px;
+  height: 48px;
+  opacity: 1;
+  border-radius: 25px;
+  border: none;
+      margin: 0 113px 48px 48px;
+`;
+Pcha3El.style.cssText = `
+  font-family: "Lexend Deca", sans-serif;
+  font-weight: 400;
+  font-size: 15px;
+  leading-trim: none;
+  line-height: 25px;
+  letter-spacing: 0px;
+  color: #004140;
+  margin:auto;
+`;
